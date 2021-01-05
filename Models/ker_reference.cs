ad4e825fcc06bd0657ba908b3e4f509bca76fe97
Jan Hrdlicka <hrdlicka.jan@gmail.com>
2021-01-05 21:04:43 +0100

    new columns ker_reference.ord ker_reftab.name

--- Models/ker_reference.cs
+++ Models/ker_reference.cs
@@ -5,10 +5,11 @@ namespace currentweather.Models
 {
     public partial class ker_reference
     {
+        public long id { get; set; }
         public string reftabnm { get; set; }
         public string namenm { get; set; }
         public string name { get; set; }
         public string description { get; set; }
-        public long id { get; set; }
+        public int ord { get; set; }
     }
 }
--- Models/ker_reftab.cs
+++ Models/ker_reftab.cs
@@ -6,6 +6,7 @@ namespace currentweather.Models
     public partial class ker_reftab
     {
         public string reftabnm { get; set; }
+        public string name { get; set; }
         public string description { get; set; }
         public long id { get; set; }
     }
